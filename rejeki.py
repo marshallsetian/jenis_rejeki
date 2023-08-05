537eddffcb80a2ff85c194f624306defbb2941ab
marshall setian <marshallsetian@gmail.com>
2023-08-06 06:52:21 +0700

    Update rejeki.py

--- rejeki.py
+++ rejeki.py
@@ -10,22 +10,22 @@ def mengetik(s):
         time.sleep(random.random() * 0.2)
 
 
-mengetik(Style.BRIGHT+Fore.GREEN+"\n8 Jenis Rejeki dari ALLAH :")
+mengetik(Style.BRIGHT+Fore.GREEN+"\n   8 Jenis Rejeki dari ALLAH :")
 mengetik(Fore.LIGHTWHITE_EX+"...............................................")
 
-mengetik(Fore.LIGHTYELLOW_EX +"1.Rejeki yang telah diJamin... ( Hud : 6 )\n")
-mengetik(Fore.LIGHTYELLOW_EX +"2.Rejeki karena Bekerja ... ( An - Najm : 38 )\n")
-mengetik("3.Rejeki karena Bersyukur ... ( Ibrahim : 7 )\n")
-mengetik("4.Rejeki tak Terduga ... ( At - Thalaq : 2-3 )\n")
-mengetik("5.Rejeki karena Istigfar ... ( Nuh : 10-11 )\n")
-mengetik("6.Rejeki karena Menikah ... ( An - Nur : 32 )\n")
-mengetik("7.Rejeki karena Anak ... ( Al - Israa : 31 )\n")
-mengetik("8.Rejeki karena Sedekah ... ( Al - Baqarah : 245 )\n\n")
+mengetik(Fore.LIGHTYELLOW_EX +"   1.Rejeki yang telah diJamin... ( Hud : 6 )\n")
+mengetik(Fore.LIGHTYELLOW_EX +"   2.Rejeki karena Bekerja ... ( An - Najm : 38 )\n")
+mengetik("   3.Rejeki karena Bersyukur ... ( Ibrahim : 7 )\n")
+mengetik("   4.Rejeki tak Terduga ... ( At - Thalaq : 2-3 )\n")
+mengetik("   5.Rejeki karena Istigfar ... ( Nuh : 10-11 )\n")
+mengetik("   6.Rejeki karena Menikah ... ( An - Nur : 32 )\n")
+mengetik("   7.Rejeki karena Anak ... ( Al - Israa : 31 )\n")
+mengetik("   8.Rejeki karena Sedekah ... ( Al - Baqarah : 245 )\n\n")
 
-mengetik(Fore.GREEN +"semoga semua Saudara & Teman2 di limpahkan Rejekinya\n")
+mengetik(Fore.GREEN +"   semoga semua Saudara & Teman2 di limpahkan Rejekinya\n")
 
-print(Fore.LIGHTWHITE_EX+"ini adalah akhir dari :")
-mengetik(Fore.LIGHTBLUE_EX+"PROGRAM PYTHON 3.9 BY.MARSHALL_SETIAN")
+print(Fore.LIGHTWHITE_EX+"   ini adalah akhir dari :")
+mengetik(Fore.LIGHTBLUE_EX+"   PROGRAM PYTHON 3.9 BY.MARSHALL_SETIAN")
 
 
 
